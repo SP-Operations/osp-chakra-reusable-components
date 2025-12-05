fccb9f608a7792876eeadb1cd3d7bc105fa49f93
Jimwell <jimwelllo@stpeter.com.ph>
2025-12-05 11:02:33 +0800

    Remove padding padding again

--- src/pages/file-claim-page/FileClaimPage.tsx
+++ src/pages/file-claim-page/FileClaimPage.tsx
@@ -220,12 +220,12 @@ export const FileClaimPage = (params: PageParams) => {
             <Steps.Root defaultStep={0} count={stepTitles.length} step={stepNumber} onStepChange={(e) => setStepNumber(e.step)}>
                 {(pageNumber < 5) && (
                     <>
-                        <Container>
+                        <Container padding="0">
                             <Text textStyle="2xl" fontWeight="semibold">Claim Application</Text>
                             <Body color="gray.600" mt={1}>Your claim matters. We make it easy.</Body>
                         </Container>
 
-                        <Container>
+                        <Container padding="0">
                             <Steps.List>
                                 {stepTitles.map((step, index) => (
                                     <Steps.Item key={index} index={index} title={step} colorPalette="green">
